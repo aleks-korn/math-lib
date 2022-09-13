1b0f44a45a948b44b4abbf3a94e91b8a0a855061
o.korniienko <o.korniienko@gmsadmins-MacBook-Pro.local>
2022-09-13 11:32:40 +0200

    comm

---
+++ MyMath2/MathTest.swift
@@ -0,0 +1,21 @@
+//
+//  MathTest.swift
+//  MathLib
+//
+//  Created by o.korniienko on 06.09.22.
+//
+
+
+import Foundation
+
+class MathTest {
+    
+    
+    
+    func testMath(){
+        let result1 = MyMath.plus(number1: 3, number2: 12)
+        print(result1)
+    }
+    
+    
+}
---
+++ MyMath2/MyMath2.h
@@ -0,0 +1,18 @@
+//
+//  MyMath2.h
+//  MyMath2
+//
+//  Created by o.korniienko on 13.09.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyMath2.
+FOUNDATION_EXPORT double MyMath2VersionNumber;
+
+//! Project version string for MyMath2.
+FOUNDATION_EXPORT const unsigned char MyMath2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyMath2/PublicHeader.h>
+
+
